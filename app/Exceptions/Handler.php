0e6a2d395a0ca8460808aa9bb5927d6103a5840d
Paulo Caldeira <paulo.caldeira@acin.pt>
2017-03-03 11:22:22 +0000

    Redirect to documentation

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,8 +45,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException)
-        return redirect('/');
+        if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+            return redirect('documentation/v1\/');
+        }
 
         return parent::render($request, $e);
     }
