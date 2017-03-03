9c63301cc8e1d70b9937cf61055a491d0a4799f2
Paulo Caldeira <paulo.caldeira@acin.pt>
2017-03-03 11:24:00 +0000

    Missing page redirects removed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,10 +45,6 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
-            return redirect('documentation/v1\/');
-        }
-
         return parent::render($request, $e);
     }
 }
